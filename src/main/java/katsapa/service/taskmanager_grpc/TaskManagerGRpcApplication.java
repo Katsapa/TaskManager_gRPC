43500be3b201d3e4abec0127c5512b5c51bf62b2
Katsapa <mramosmagic@gmail.com>
2026-05-04 21:01:47 +0300

    init commit

---
+++ src/main/java/katsapa/service/taskmanager_grpc/TaskManagerGRpcApplication.java
@@ -0,0 +1,13 @@
+package katsapa.service.taskmanager_grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerGRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerGRpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/katsapa/service/taskmanager_grpc/TaskManagerGRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package katsapa.service.taskmanager_grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerGRpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
